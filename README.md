3ad31349866aac4611e493bd033343ae1fac149b
Florent BOURASSE <fbourasse@jahia.com>
2023-10-07 08:42:29 +0200

    First commit
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Server Availability Manager - Cluster Revisions check
+
+The purpose of this module is to add a probe to the Server Availability Manager to check if the cluster revisions are in sync and not "blocked"
+
+## Installation
+
+- In Jahia, go to "Administration --> Server settings --> System components --> Modules"
+- Upload the JAR **sam-cluster-revisions-check-X.X.X.jar**
+- Check that the module is started
+
+## How to use
+The probe will be automatically added to the Server Availability Manager output (read [this page](https://academy.jahia.com/documentation/system-administrator/dev-ops/monitoring-your-jahia-platform/monitoring-your-servers) for more information).
---
+++ src/main/java/org/jahia/community/sam/clusterrevisionscheck/ClusterRevisionsProbe.java
@@ -0,0 +1,108 @@
+package org.jahia.community.sam.clusterrevisionscheck;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import org.apache.jackrabbit.core.util.db.ConnectionHelper;
+import org.apache.jackrabbit.core.util.db.DbUtility;
+import org.jahia.modules.sam.Probe;
+import org.jahia.modules.sam.ProbeStatus;
+import org.osgi.service.component.annotations.Component;
+
+import org.jahia.modules.sam.ProbeSeverity;
+import org.jahia.utils.DatabaseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true, service = Probe.class)
+public class ClusterRevisionsProbe implements Probe {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClusterRevisionsProbe.class);
+    private Long lastRevisionId = Long.MAX_VALUE;
+    private String lastJournalId = "";
+    private String schemaObjectPrefix = "JR_J_";
+    private String sqlStmtSelectGlobalRevision;
+    private String sqlStmtSelectMinimumRevision;
+    private final ConnectionHelper connHelper;
+    private long checkInterval = 15L;
+    private long lastCheckTimestamp = Long.MIN_VALUE;
+//INSERT INTO JR_J_LOCAL_REVISIONS (JOURNAL_ID,REVISION_ID) VALUES('test', 1)
+    public ClusterRevisionsProbe() {
+        connHelper = new ConnectionHelper(DatabaseUtils.getDatasource(), false);
+    }
+
+    @Override
+    public String getName() {
+        return "ClusterRevisionsCheck";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Checking that the cluster revisions aren't out-of-sync";
+    }
+
+    @Override
+    public ProbeStatus getStatus() {
+        ProbeStatus status = new ProbeStatus("Cluster revisions is sync", ProbeStatus.Health.GREEN);
+        final long currentCheckTimestamp = System.currentTimeMillis();
+
+        if (currentCheckTimestamp >= lastCheckTimestamp + checkInterval * 1000L) {
+            lastCheckTimestamp = currentCheckTimestamp;
+            try {
+                long globalRevision = getQueryLong(sqlStmtSelectGlobalRevision);
+                ResultSet rs = null;
+                try {
+                    rs = connHelper.exec(sqlStmtSelectMinimumRevision, new Object[]{globalRevision}, false, 1);
+                    if (rs.next()) {
+                        final String journalId = rs.getString(1);
+                        final Long revisionId = rs.getLong(2);
+                        if (journalId.equals(lastJournalId) && revisionId.equals(lastRevisionId)) {
+                            status = new ProbeStatus(String.format("The following node seems out-of-sync: %s (%s)", lastJournalId, lastRevisionId.toString()), ProbeStatus.Health.RED);
+                        } else {
+                            lastJournalId = journalId;
+                            lastRevisionId = revisionId;
+                        }
+                    }
+                } finally {
+                    DbUtility.close(rs);
+                }
+            } catch (SQLException ex) {
+                LOGGER.error("Issue when executing SQL statement", ex);
+            }
+
+        }
+        return status;
+    }
+
+    @Override
+    public ProbeSeverity getDefaultSeverity() {
+        return ProbeSeverity.HIGH;
+    }
+
+    @Override
+    public void setConfig(Map<String, Object> config) {
+        if (config.containsKey("check_interval")) {
+            checkInterval = Long.parseLong(config.get("check_interval").toString());
+        }
+        if (config.containsKey("schemaObjectPrefix")) {
+            schemaObjectPrefix = config.get("schemaObjectPrefix").toString();
+        }
+        sqlStmtSelectGlobalRevision = "select REVISION_ID from " + schemaObjectPrefix + "GLOBAL_REVISION";
+        sqlStmtSelectMinimumRevision = "SELECT JOURNAL_ID, REVISION_ID FROM " + schemaObjectPrefix + "LOCAL_REVISIONS WHERE REVISION_ID < ? ORDER BY REVISION_ID ASC";
+    }
+
+    private long getQueryLong(String query, Object... params) throws SQLException {
+        ResultSet rs = null;
+        long result = 0;
+        try {
+            rs = connHelper.exec(query, params, false, 0);
+            if (rs.next()) {
+                result = rs.getLong(1);
+            }
+        } finally {
+            DbUtility.close(rs);
+        }
+        return result;
+    }
+
+}
